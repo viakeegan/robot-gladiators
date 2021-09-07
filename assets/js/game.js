486c7a4a0f2f318a23996a83ddb85ac5eccaee15
keegan jervis <viakeegan@gmail.com>
2021-09-07 02:02:01 -0400

    console log player name

---
+++ assets/js/game.js
@@ -0,0 +1,19 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// What is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight" //
+function fight () {
+window.alert("The fight has begun!");
+}
+
+// fight();
+
+
+
+
