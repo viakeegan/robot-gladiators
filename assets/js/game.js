c47797031c97a1e23c4bc28987a94c8fecbdc148
keegan jervis <viakeegan@gmail.com>
2021-09-07 03:49:24 -0400

    add player health

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,16 +20,31 @@ var fight = function() {
     // log a resulting message to the console so we know that it worked.
     console.log(
         playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
-    );   
+    );
+
+    // check enemy health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " was decemated! ");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
     playerHealth =  playerHealth - enemyAttack;
 
     // Log a resulting message to the console so we know that it worked. 
     console.log(
-        enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaing."
-    )
-};
+        enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaing." );
+    
+    // check player health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " was decemated! ");
+    }
+    else { 
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    };
 
 // execute function
 fight();
